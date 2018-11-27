4b16f549e0897861ca0018094d5b417e432312c8
kchristou <kchristou@uclan.ac.uk>
2018-11-27 19:19:47 +0200

    Continues the previous commit

--- TreasureHunt.js
+++ TreasureHunt.js
@@ -38,7 +38,7 @@ function list() {
                 var linkElement = document.createElement("a");
                 linkElement.innerHTML = object.treasureHunts[i].name;
                 linkElement.href = "PlayPage.html";
-                THchallenge.appendChild(newTreasureHunt);
+               // THchallenge.appendChild(newTreasureHunt);
                 newTreasureHunt.appendChild(linkElement);
 
             }
@@ -50,33 +50,42 @@ function list() {
 
     xhttp.open("GET", "https://codecyprus.org/th/api/list", true);
     xhttp.send();
-
     function Submit() {
-        document.getElementById("Submit").onclick = function saveCredentials() {
-            var Username = document.getElementById("Username");
-            var appName = document.getElementById("AppName");
-            start(Username.value, appName.value);
+        document.getElementsByClassName("Button").onclick = function saveCredentials() {
+            var Username = document.getElementsByClassName("Username");
+            var appName = document.getElementsByClassName("AppName");
+
         };
     }
+
+
 }
-function start(Username, appName){
+function start(Username,appName){
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
             //TODO If response received (success).
             object = JSON.parse(this.responseText);
-            if(object.status==="error"){
-            alert(object.errorMessege);
-
-            } else{
-                document.cookie=object.session;
-                window.location.href="AnswerSheet.html";
+            window.location.href = "AnswerSheet.html";
+            if (object.status === "error") {
+                alert(object.errorMessage);
+            } else
+            {
+                document.cookie = object.session;
+                window.location.href = "AnswerSheet.html";
+                console.log("username"+Username,"appname"+appName);
             }
-        }
-        }  ;
-    xhttp.open("GET", "https://codecyprus.org/th/api/start?player= "+uName + "Appname= "+appName+"&treasure-hunt-id="+getCookie("uuid"));
-    xhttp.send();
 
+            }
+
+        else
+            {
+                //TODO If response not received (error).
+            }
+
+    };
+    xhttp.open("GET", "https://codecyprus.org/th/api/start?player= "+ Username + "app= "+appName+"&treasure-hunt-id=ag9nfmNvZGVjeXBydXNvcmdyGQsSDFRyZWFzdXJlSHVudBiAgICAvKGCCgw");
+    xhttp.send();
 }
 
 function questions(){
@@ -85,11 +94,15 @@ function questions(){
         if (this.readyState === 4 && this.status === 200) {
             //TODO If response received (success).
             object = JSON.parse(this.responseText);
-            if(object.currentQuestionIndex === object.numOfQuestions){
-
-
-
-
+            console.log(this.responseText);
+            var THchallenge = document.getElementById("Sample")
+            for (var i = 0; i < object.treasureHunts.length; i++) {
+                var newTreasureHunt = document.createElement("p");
+                var linkElement = document.createElement("a");
+                linkElement.innerHTML = object.treasureHunts[i].name;
+               // linkElement.href = "LeaderBoard.html";
+                THchallenge.appendChild(newTreasureHunt);
+                newTreasureHunt.appendChild(linkElement);
             }
         }
         else {
@@ -100,10 +113,10 @@ function questions(){
     xhttp.send();
 
 }
-console.log(Submit(Username,appName));
-questions();
 
+//questions();
 //list();
 //start();
+
 //LeaderBoard();
 //console.log(document.cookie);
